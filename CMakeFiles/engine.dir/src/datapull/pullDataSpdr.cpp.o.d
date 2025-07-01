8928956f8245f8c235e81ce3daee2e3de4c2d2cc
Theo <theowimber@gmail.com>
2025-07-01 09:46:49 +0200

    removed CMakeFiles

--- CMakeFiles/engine.dir/src/datapull/pullDataSpdr.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/engine.dir/src/datapull/pullDataSpdr.cpp.o: \
-  /Users/theo/Desktop/Code-Projekte/engine/src/datapull/pullDataSpdr.cpp \
-  /Users/theo/Desktop/Code-Projekte/engine/src/datapull/pullDataSpdr.h
--- CMakeFiles/engine.dir/src/datapull/pullDataUsr.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/engine.dir/src/datapull/pullDataUsr.cpp.o: \
-  /Users/theo/Desktop/Code-Projekte/engine/src/datapull/pullDataUsr.cpp \
-  /Users/theo/Desktop/Code-Projekte/engine/src/datapull/pullDataUsr.h
