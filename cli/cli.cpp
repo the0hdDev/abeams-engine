78039cdfd2a32a6a4b222c039361738be0047dc9
Theo <theowimber@gmail.com>
2025-09-07 14:48:24 +0200

    working on implementing the functions

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -1,28 +1,22 @@
 #include "cli.h"
-
-
-void printHelp()
-{
-
-}
-
-void printVersion()
-{
-
-}
-
-void shutdown()
-{
-
-}
-
+#include "cli_Implementations.h"
 
 
 cli::cli()
 {
-
     commandMap["help"] = printHelp;
-    commandMap["version"] = printHelp;
+    commandMap["version"] = printVersion;
+    commandMap["shutdown"] = shutdown;
+    commandMap["exit"] = shutdown;
+    commandMap["poweroff"] = shutdown;
+    commandMap["quit"] = shutdown;
+    commandMap["restart"] = restart;
+    commandMap["status"] = printStatus;
+    commandMap["config"] = configHandler;
+    commandMap["clear"] = clearCLI;
+    commandMap["echo"] = [](int argc, char* argv[]){  echo(argc, argv);  };
+    commandMap["simulation"] = startCLISimulationHandler;
+    commandMap["sim"] = startCLISimulationHandler;
 
 
 }
---
+++ cli/cli_Implementations.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+void printHelp()
+{
+
+}
+
+void printVersion()
+{
+
+}
+
+void shutdown()
+{
+
+}
+
+void restart()
+{
+
+}
+void printStatus()
+{
+
+}
+
+void clearCLI()
+{
+
+}
+
+void echo(int argc, char* argv[])
+{
+
+}
